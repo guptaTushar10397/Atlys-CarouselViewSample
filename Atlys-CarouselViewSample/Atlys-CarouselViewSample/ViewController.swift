93bc2a9a48c2d167c4795a4b1dace225aed3aee8
Tushar Gupta <tushar.gupta@noice.id>
2024-09-22 16:29:52 +0530

    Project created.

---
+++ Atlys-CarouselViewSample/Atlys-CarouselViewSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Atlys-CarouselViewSample
+//
+//  Created by Tushar Gupta on 22/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
